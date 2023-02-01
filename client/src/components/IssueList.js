15c46640ded00c4b28aeb868347c3d95263b0bdb
noah berlove <noah.berlove@gmail.com>
2023-01-31 20:23:58 -0800

    updates for hosting

--- client/src/components/IssueList.js
+++ client/src/components/IssueList.js
@@ -1,7 +1,5 @@
 import React from "react";
 import Issue from './Issue'
-import {useContext} from "react"
-import IssueProvider from "../context/IssueProvider";
 
 export default function IssueList(props) {
 
